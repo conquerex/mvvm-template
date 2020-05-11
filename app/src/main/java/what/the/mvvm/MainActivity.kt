e9de4e584c903514d46ae50ee18d3c1c9a879d2d
Jongkook <conquerex@gmail.com>
2020-05-11 21:41:38 +0900

    5. MainActivity

--- app/src/main/java/what/the/mvvm/MainActivity.kt
+++ app/src/main/java/what/the/mvvm/MainActivity.kt
@@ -1,12 +1,29 @@
 package what.the.mvvm
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import what.the.mvvm.base.BaseActivity
+import what.the.mvvm.databinding.ActivityMainBinding
+import what.the.mvvm.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+
+    override val viewModel: MainViewModel by viewModel()
+
+    // todo : mainSearchRecyclerViewAdapter
+
+    override fun initStartView() {
+        // todo : main_activity_search_recycler_view.run
     }
+
+    override fun initDataBinding() {
+        // todo : viewModel.imageSearchResponseLiveData
+    }
+
+    override fun initAfterBinding() {
+        // todo : main_activity_search_button.setOnClickListener
+    }
+
 }
