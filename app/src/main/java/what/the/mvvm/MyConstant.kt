8d3afae53e003a241fc7ecc8143ea40010a6d63f
Jongkook <conquerex@gmail.com>
2020-05-13 15:16:06 +0900

    6. RecyclerView

---
+++ app/src/main/java/what/the/mvvm/MyConstant.kt
@@ -0,0 +1,3 @@
+package what.the.mvvm
+
+public val KAKAO_APP_KEY = "54b77daffb3fe905570986fd93aec7e2"
--- app/src/main/java/what/the/mvvm/model/DataModelImpl.kt
+++ app/src/main/java/what/the/mvvm/model/DataModelImpl.kt
@@ -1,13 +1,12 @@
 package what.the.mvvm.model
 
 import io.reactivex.Single
+import what.the.mvvm.KAKAO_APP_KEY
 import what.the.mvvm.model.enum.KakaoSearchSortEnum
 import what.the.mvvm.model.response.ImageSearchResponse
 import what.the.mvvm.model.service.KakaoSearchService
 
 class DataModelImpl(private val service: KakaoSearchService) : DataModel {
-
-    private val KAKAO_APP_KEY = "54b77daffb3fe905570986fd93aec7e2"
 
     override fun getData(query: String, sort: KakaoSearchSortEnum, page: Int, size: Int): Single<ImageSearchResponse> {
         return service.searchImage(auth = "KakaoAK $KAKAO_APP_KEY", query = query, sort = sort.sort, page = page, size = size)
